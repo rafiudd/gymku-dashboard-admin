10ea81fe65c9131bfa2e945e7ee7d2a71a1786c4
rafiudd <sirin6867@gmail.com>
2020-01-19 21:24:05 +0700

    add login

---
+++ src/views/examples/Tables.jsx
@@ -0,0 +1,167 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+import {
+  Badge,
+  Card,
+  CardHeader,
+  CardFooter,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  Media,
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+  Progress,
+  Table,
+  Container,
+  Row,
+  UncontrolledTooltip
+} from "reactstrap";
+// core components
+import Header from "components/Headers/Header.jsx";
+
+class Tables extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+        {/* Page content */}
+        <Container className="mt--8" fluid>
+          {/* Table */}
+          <Row>
+            <div className="col">
+              <Card className="shadow">
+                <CardHeader className="border-0">
+                  <h3 className="mb-0">Daftar User</h3>
+                </CardHeader>
+                <Table className="align-items-center table-flush" responsive>
+                  <thead className="thead-light">
+                    <tr>
+                      <th scope="col">Fullname</th>
+                      <th scope="col">Email</th>
+                      <th scope="col">Phone</th>
+                      <th scope="col">Gender</th>
+                      <th scope="col">Class</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td className="text-sm"><a href="#" target="_blank" rel="noopener noreferrer">Rayhan Rafiud Darojat</a></td>
+                      <td>rayhanrafiudd@gmail.com</td>
+                      <td>088983210303</td>
+                      <td>Laki Laki</td>
+                      <td>Atletik</td>                      
+                    </tr>
+                    <tr>
+                      <td className="text-sm"><a href="#" target="_blank" rel="noopener noreferrer">Rayhan Rafiud Darojat</a></td>
+                      <td>rayhanrafiudd@gmail.com</td>
+                      <td>088983210303</td>
+                      <td>Laki Laki</td>
+                      <td>Atletik</td>                      
+                    </tr>
+                    <tr>
+                      <td className="text-sm"><a href="#" target="_blank" rel="noopener noreferrer">Rayhan Rafiud Darojat</a></td>
+                      <td>rayhanrafiudd@gmail.com</td>
+                      <td>088983210303</td>
+                      <td>Laki Laki</td>
+                      <td>Atletik</td>                      
+                    </tr>
+                    <tr>
+                      <td className="text-sm"><a href="#" target="_blank" rel="noopener noreferrer">Rayhan Rafiud Darojat</a></td>
+                      <td>rayhanrafiudd@gmail.com</td>
+                      <td>088983210303</td>
+                      <td>Laki Laki</td>
+                      <td>Atletik</td>                      
+                    </tr>
+                    <tr>
+                      <td className="text-sm"><a href="#" target="_blank" rel="noopener noreferrer">Rayhan Rafiud Darojat</a></td>
+                      <td>rayhanrafiudd@gmail.com</td>
+                      <td>088983210303</td>
+                      <td>Laki Laki</td>
+                      <td>Atletik</td>                      
+                    </tr>
+                  </tbody>
+                </Table>
+                <CardFooter className="py-4">
+                  <nav aria-label="...">
+                    <Pagination
+                      className="pagination justify-content-end mb-0"
+                      listClassName="justify-content-end mb-0"
+                    >
+                      <PaginationItem className="disabled">
+                        <PaginationLink
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                          tabIndex="-1"
+                        >
+                          <i className="fas fa-angle-left" />
+                          <span className="sr-only">Previous</span>
+                        </PaginationLink>
+                      </PaginationItem>
+                      <PaginationItem className="active">
+                        <PaginationLink
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          1
+                        </PaginationLink>
+                      </PaginationItem>
+                      <PaginationItem>
+                        <PaginationLink
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          2 <span className="sr-only">(current)</span>
+                        </PaginationLink>
+                      </PaginationItem>
+                      <PaginationItem>
+                        <PaginationLink
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          3
+                        </PaginationLink>
+                      </PaginationItem>
+                      <PaginationItem>
+                        <PaginationLink
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <i className="fas fa-angle-right" />
+                          <span className="sr-only">Next</span>
+                        </PaginationLink>
+                      </PaginationItem>
+                    </Pagination>
+                  </nav>
+                </CardFooter>
+              </Card>
+            </div>
+          </Row>
+          {/* Dark table */}
+        </Container>
+      </>
+    );
+  }
+}
+
+export default Tables;
